932b2b81dff36b8465772d2d4de9a5ba03cce485
EliasDev <eliasbonnin99@gmail.com>
2025-05-22 16:24:50 -0300

    Empezamos el ejercicio extra 11

--- Roadmap/11 - MANEJO DE FICHEROS/python/EliasBonnin.py
+++ Roadmap/11 - MANEJO DE FICHEROS/python/EliasBonnin.py
@@ -19,4 +19,54 @@ with open(nombre_archivo, "r") as archivo:
 os.remove(nombre_archivo)
 
 
+# Limpiar Consola
+
+def limpiar_consola():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 # Extra
+
+
+archivo_local = "GestionVentas.txt"
+open(archivo_local, "a")
+
+
+def mostrar_menu():
+    print("\n Gestion de Productos")
+    print("1. insertar Producto")
+    print("2. Actualizar Producto")
+    print("3. Buscar Producto")
+    print("4. Borrar Producto")
+    print("5. Calcular Venta por Producto")
+    print("6. Calcular Venta Total")
+    print("7. Salir\n")
+
+
+while True:
+    limpiar_consola()
+    mostrar_menu()
+
+    action = input("Elegir Opcion:  ")
+
+    if action == "1":
+        limpiar_consola()
+        nombre = input("Ingrese nombre de producto:  ")
+        cantidad = input("Ingrese cantidad vendida:  ")
+        precio = input("Ingrese el precio:  ")
+        with open(archivo_local, "a") as archivo:
+            archivo.write(f"{nombre}, {cantidad}, {precio}")
+    elif action == "2":
+        pass
+    elif action == "3":
+        pass
+    elif action == "4":
+        pass
+    elif action == "5":
+        pass
+    elif action == "6":
+        pass
+    elif action == "7":
+        os.remove(archivo_local)
+        break
+    else:
+        print("Seleccione solo las opciones disponibles")
